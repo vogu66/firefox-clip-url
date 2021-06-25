449d2a95b54d4d6ac76ef2ee4aad135238c91ed6
vogu66 <24728720+vogu66@users.noreply.github.com>
2021-06-25 11:36:36 +0200

    1.2 with changing icon

--- copied.js
+++ copied.js
@@ -1,18 +1,15 @@
-browser.pageAction.onClicked.addListener(() => {
-	var querying = browser.tabs.query({currentWindow: true, active: true});
-	querying.then((tabs) => 
+browser.pageAction.onClicked.addListener((tab) => {
+	var prom = navigator.clipboard.writeText(tab.url);
+	prom.then( () =>
 	{
-		var promise = navigator.clipboard.writeText(tabs[0].url);
-		/*
-		promise.then( () => 
-		{
-			// copied
-			browser.pageAction.setIcon({tabID: tabs[0].url, path:"icons/link-icon-red.svg"})
-		}, () => 
-		{
-			// not copied
-			browser.pageAction.setIcon({tabID: tabs[0].url, path:"icons/link-icon-red.svg"})
-		} );
-		*/
-	})
+		// copied
+		browser.pageAction.setIcon({tabId: tab.id, path: "icons/like.svg"});
+	}, () =>
+	{
+		// not copied
+		browser.pageAction.setIcon({tabId: tab.id, path: "icons/unlike.svg"});
+	} );
+	setTimeout(function(){
+		browser.pageAction.setIcon({tabId: tab.id, path: "icons/link-icon.svg"});
+	},1000);
 });
